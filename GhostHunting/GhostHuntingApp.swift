fd89d7ac5aecf41b45f1d93186daf6610518b8c1
dary-winata <dary.winata15@gmail.com>
2023-08-03 23:47:52 +0700

    Initial Commit

---
+++ GhostHunting/GhostHuntingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhostHuntingApp.swift
+//  GhostHunting
+//
+//  Created by dary winata nugraha djati on 03/08/23.
+//
+
+import SwiftUI
+
+@main
+struct GhostHuntingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
